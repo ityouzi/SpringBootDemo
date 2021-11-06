9820b9b562b23bae4769f5d59b1ead96a8a92a83
lizhen <2504480136@qq.com>
2021-11-06 23:05:29 +0800

    [lz]Java8 Stream流递归，几行代码搞定遍历树形结构

--- src/main/java/com/example/springbootdemo/util/SimpleApp.java
+++ src/main/java/com/example/springbootdemo/util/SimpleApp.java
@@ -1,7 +1,5 @@
 package com.example.springbootdemo.util;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.Field;
@@ -60,20 +58,83 @@ public class SimpleApp {
     }
 
     public static void main(String[] args) {
+//        Double a = new Double(8000.98000);
+//        Student student = new Student("张三",a);
+//
+//        System.out.println(JSON.toJSONString(student));
+//
+//        System.out.println(BigDecimal.valueOf(a));
 
-        String a = "\n" +
-                "可点击key和value值进行编辑\n" +
-                "{\n" +
-                "    \"sign\":\"ftP/1O6AjHbCpt9J3cthu4aFsvgh46y3jDJQJCdKOUm5KGrsxQRAE1A8+7kHYAvbieIp8z+DQ/uHS+NCjExQTMapu0L95bpcJjf/v8fq/3rNfthGKELDIndepkWeRdb8jW5K53jKE77qIYGQu84JKlrJPZGupCHD/O0phn6ffXY=\",\n" +
-                "    \"ysepay_merchant_register_token_get_response\":{\n" +
-                "        \"code\":\"10000\",\n" +
-                "        \"msg\":\"Success\",\n" +
-                "        \"token\":\"TK20211027155522124BRSoOUUD\",\n" +
-                "        \"token_status\":\"TOKEN_GET_SUCCESS\"\n" +
-                "    }\n";
-
-        JSONObject jsonObject = JSON.parseObject(a, JSONObject.class);
-        System.out.println(jsonObject.get("token"));
+        String a = "MERCHANT_TYPE_(\"100\",\"实物d\n" +
+                "MERCHANT_TYPE_(\"110\",\"综合商城d\n" +
+                "MERCHANT_TYPE_(\"111\",\"超市（线下）d\n" +
+                "MERCHANT_TYPE_(\"121\",\"服饰类商城/服饰配件/箱包d\n" +
+                "MERCHANT_TYPE_(\"122\",\"礼品/鲜花/纪念品d\n" +
+                "MERCHANT_TYPE_(\"123\",\"户外/运动/健身器材/安防d\n" +
+                "MERCHANT_TYPE_(\"124\",\"乐器d\n" +
+                "MERCHANT_TYPE_(\"125\",\"手表/钟表/眼睛（美瞳）d\n" +
+                "MERCHANT_TYPE_(\"126\",\"黄金珠宝/钻石/玉石d\n" +
+                "MERCHANT_TYPE_(\"127\",\"首饰/饰品d\n" +
+                "MERCHANT_TYPE_(\"128\",\"装饰/设计d\n" +
+                "MERCHANT_TYPE_(\"131\",\"家居/建材/装饰/布艺类商城d\n" +
+                "MERCHANT_TYPE_(\"132\",\"食品d\n" +
+                "MERCHANT_TYPE_(\"133\",\"保健品/滋补品d\n" +
+                "MERCHANT_TYPE_(\"134\",\"家用电器d\n" +
+                "MERCHANT_TYPE_(\"135\",\"成人计生用品d\n" +
+                "MERCHANT_TYPE_(\"136\",\"美妆/护肤/个人护理d\n" +
+                "MERCHANT_TYPE_(\"137\",\"工艺品/盆栽/室内装饰品d\n" +
+                "MERCHANT_TYPE_(\"138\",\"汽车/摩托/自行车/配件/改装d\n" +
+                "MERCHANT_TYPE_(\"139\",\"餐饮d\n" +
+                "MERCHANT_TYPE_(\"141\",\"数码产品d\n" +
+                "MERCHANT_TYPE_(\"142\",\"办公设备（打印机/复印机/扫描仪等）d\n" +
+                "MERCHANT_TYPE_(\"150\",\"母婴/玩具d\n" +
+                "MERCHANT_TYPE_(\"154\",\"培训教育d\n" +
+                "MERCHANT_TYPE_(\"155\",\"医药d\n" +
+                "MERCHANT_TYPE_(\"156\",\"摄影d\n" +
+                "MERCHANT_TYPE_(\"161\",\"影票/演唱会/赛事等娱乐票务d\n" +
+                "MERCHANT_TYPE_(\"162\",\"火车票/船票/车票等长途交通票务d\n" +
+                "MERCHANT_TYPE_(\"170\",\"票务/机票/旅游d\n" +
+                "MERCHANT_TYPE_(\"171\",\"旅行社d\n" +
+                "MERCHANT_TYPE_(\"172\",\"旅游服务平台d\n" +
+                "MERCHANT_TYPE_(\"173\",\"交通运输服务类d\n" +
+                "MERCHANT_TYPE_(\"175\",\"旅馆/酒店/景区/度假区d\n" +
+                "MERCHANT_TYPE_(\"180\",\"书籍/音像/文具d\n" +
+                "MERCHANT_TYPE_(\"190\",\"收藏/宠物/文物/非文物d\n" +
+                "MERCHANT_TYPE_(\"210\",\"彩票d\n" +
+                "MERCHANT_TYPE_(\"220\",\"生活/咨询服务d\n" +
+                "MERCHANT_TYPE_(\"221\",\"人才中介机构/招聘/猎头d\n" +
+                "MERCHANT_TYPE_(\"222\",\"婚庆/婚介d\n" +
+                "MERCHANT_TYPE_(\"223\",\"网上生活服务平台d\n" +
+                "MERCHANT_TYPE_(\"230\",\"金融d\n" +
+                "MERCHANT_TYPE_(\"240\",\"保险/保险公司d\n" +
+                "MERCHANT_TYPE_(\"250\",\"公益d\n" +
+                "MERCHANT_TYPE_(\"260\",\"网络虚拟服务/软件/彩铃d\n" +
+                "MERCHANT_TYPE_(\"263\",\"机械设备/电子d\n" +
+                "MERCHANT_TYPE_(\"265\",\"通信/宽带/话费充值d\n" +
+                "MERCHANT_TYPE_(\"280\",\"娱乐/健身服务d\n" +
+                "MERCHANT_TYPE_(\"290\",\"苗木/绿化/化肥d\n" +
+                "MERCHANT_TYPE_(\"291\",\"快递/货运服务d\n" +
+                "MERCHANT_TYPE_(\"292\",\"水电煤缴费/交通罚款等生活缴费d\n" +
+                "MERCHANT_TYPE_(\"293\",\"公立医院d\n" +
+                "MERCHANT_TYPE_(\"294\",\"民办院校（中学/小学/幼儿园）d\n" +
+                "MERCHANT_TYPE_(\"295\",\"挂号平台d\n" +
+                "MERCHANT_TYPE_(\"296\",\"公立院校d\n" +
+                "MERCHANT_TYPE_(\"297\",\"停车缴费d\n" +
+                "MERCHANT_TYPE_(\"298\",\"事业单位d\n" +
+                "MERCHANT_TYPE_(\"299\",\"物业管理费d\n" +
+                "MERCHANT_TYPE_(\"300\",\"其他生活缴费d\n" +
+                "MERCHANT_TYPE_(\"301\",\"教育/培训/考试缴费/学费d\n" +
+                "MERCHANT_TYPE_(\"302\",\"私立院校d\n" +
+                "MERCHANT_TYPE_(\"303\",\"诊所/卫生站/卫生服务中心d\n" +
+                "MERCHANT_TYPE_(\"304\",\"咨询/法律咨询/金融咨询等d\n" +
+                "MERCHANT_TYPE_(\"305\",\"宗教d\n" +
+                "MERCHANT_TYPE_(\"310\",\"[个体户]美妆/护肤/个人护理d\n";
+        List<String> d = Arrays.asList(a.split("d"));
+        for (int i=0; i<d.size(); i++){
+            String s = d.get(i);
+            String ss = s.replace("MERCHANT_TYPE_", "MERCHANT_TYPE_" + i);
+            System.out.println(ss+"g");
+        }
     }
 
 }
